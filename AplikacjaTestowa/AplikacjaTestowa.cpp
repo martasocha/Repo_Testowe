43663c690607eee67db7193d5dc83227753bc75f
LidiaJarmolkiewicz <lidiajarmolkiewicz@gmail.com>
2022-10-10 21:00:05 +0200

    pierwsze

--- AplikacjaTestowa/AplikacjaTestowa.cpp
+++ AplikacjaTestowa/AplikacjaTestowa.cpp
@@ -7,6 +7,9 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << ":)";
+    std::cout << "witamy w piekie³ku"<<std::endl;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
